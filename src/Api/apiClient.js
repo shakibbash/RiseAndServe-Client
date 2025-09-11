20a927017ddc7d55b5e586749c9c9489ef76a9df
shakibbash <shakibhossain2273@gmail.com>
2025-09-12 00:25:54 +0600

    updated base url again

--- src/Api/apiClient.js
+++ src/Api/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { auth } from "../../Firebase/Firebase.config";
 
 // Base URL
-const BASE_URL = "https://b11a11-server-side-shakibbash-nicipffe1.vercel.app";
+const BASE_URL = "https://b11a11-server-side-shakibbash-1qhfiq7dw.vercel.app ";
 
 // Create axios instance
 const apiClient = axios.create({
